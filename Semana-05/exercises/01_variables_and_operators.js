95e4e0a097262cb28c329dcb39f2db6e56de3809
Martin Garcia <martin-garcia95@hotmail.com>
2022-09-16 11:43:20 -0300

    BaSP-W05-2022:ex01-variables and operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,25 @@
+console.log('-- EXERCISE 1: VARIABLES AND OPERATORS');
+
+//a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+console.log('-Exercise 1.a:');
+var numerA = 15;
+var numerB = 5;
+var resultC = numerA + numerB;
+console.log(resultC);
+
+//b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+console.log('-Exercise 1.b:');
+var stringFirst = 'Hello ';
+var stringSecond = 'World';
+var stringThird = stringFirst + stringSecond;
+console.log(stringThird);
+
+//c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+console.log('-Exercise 1.c:');
+var firstName = 'Martin';
+var lastName = 'Garcia';
+var completeName = firstName + lastName;
+console.log(completeName.length);
