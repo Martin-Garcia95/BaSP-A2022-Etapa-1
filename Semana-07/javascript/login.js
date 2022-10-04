c2257e0592982b6f1c8e269198f1891c96731ee8
Martin Garcia <martin-garcia95@hotmail.com>
2022-10-04 11:02:42 -0300

    BaSP-W07-2022: Finished the project with the modal section

--- Semana-07/javascript/login.js
+++ Semana-07/javascript/login.js
@@ -3,15 +3,8 @@ window.onload = function ()  {
     var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
     var inputPassword = document.getElementById("form-input-password");
     var inputButton = document.getElementById("form-button");
-    var invalidFieldsBoth = document.getElementById("invalid-fields-both");
     var invalidFieldsEmail = document.getElementById("invalid-fields-email");
-    var invalidFieldsPassword = document.getElementById("invalid-fields-password");
-    var invalidFieldsEmpty = document.getElementById("invalid-fields-empty");
-    var span = document.getElementsByClassName("close")[0];
-    var falseEmail= "Verify email: ";
-    var falsePassword= "Verify password: ";
-    var trueEmail= "Email: ";
-    var truePassword= "Password: ";
+    var span = document.getElementsByClassName("close");
     var errorTextEmail = document.createElement("p");
     var textError = document.createElement("p");
 
@@ -26,42 +19,32 @@ window.onload = function ()  {
             }
         }
     }
-
-
     inputEmail.onblur = function(){
         if(!inputEmail.value.match(emailExpression)){
             inputEmail.classList.add("border-red");
-            errorTextEmail.classList.add("parragraph-Email")
             errorTextEmail.innerHTML = "Verify email";
             inputEmail.parentNode.insertBefore(errorTextEmail, inputEmail.nextSibling);
         }else if(inputEmail.value.length < 3){
             inputEmail.classList.add("border-red");
-            errorTextEmail.classList.add("parragraph-Email")
             errorTextEmail.innerHTML = "Not enough characters";
             inputEmail.parentNode.insertBefore(errorTextEmail, inputEmail.nextSibling)
             alert("necesita mas caracteres");
         }else{
-            console.log("Valid email");
             inputEmail.classList.add("border-green")
         }
     }
-    
     inputEmail.onfocus = function(){
         inputEmail.value = "";
         inputEmail.classList.remove("border-red");
         errorTextEmail.remove();
     }
-
-
     inputPassword.onblur = function(){
         if(!onlyNumbersAndLetters(inputPassword)){
             inputPassword.classList.add("border-red");
-                textError.classList.add("parragraph-Email")
                 textError.innerHTML = "Verify password";
                 inputPassword.parentNode.insertBefore(textError, inputPassword.nextSibling)
         }else if(inputPassword.value.length < 8){
             inputPassword.classList.add("border-red");
-            textError.classList.add("parragraph-Email")
             textError.innerHTML = "Not enough characters";
             inputPassword.parentNode.insertBefore(textError, inputPassword.nextSibling)
         }else{
@@ -72,23 +55,23 @@ window.onload = function ()  {
             inputPassword.value = "";
             inputPassword.classList.remove("border-red");
             textError.remove()
-        }
-
-    
+    }
     inputButton.onclick = function(e){
         e.preventDefault();
-        if (inputEmail.classList.contains("border-red") && inputPassword.classList.contains("border-red")){
-            invalidFieldsBoth.style.display = "block";
-                span.onclick = function() {
-                    invalidFieldsBoth.style.display = "none";
+        if(inputEmail.classList.contains("border-red") && inputPassword.classList.contains("border-red")){
+            invalidFieldsEmail.style.display = "block";
+            document.getElementById("span-alert").innerHTML="Invalid fields"
+            span.onclick = function() {
+                invalidFieldsEmail.style.display = "none";
+        }
+        window.onclick = function(event) {
+            if (event.target == invalidFieldsEmail) {
+            invalidFieldsEmail.style.display = "none";
             }
-            window.onclick = function(event) {
-                if (event.target == invalidFieldsBoth) {
-                invalidFieldsBoth.style.display = "none";
-                }
-            }
+        }
         }else if(inputEmail.classList.contains("border-red")){
             invalidFieldsEmail.style.display = "block";
+            document.getElementById("span-alert").innerHTML="Invalid email"
             span.onclick = function() {
                 invalidFieldsEmail.style.display = "none";
         }
@@ -98,34 +81,33 @@ window.onload = function ()  {
             }
         }
         }else if(inputPassword.classList.contains("border-red")){
-            invalidFieldsPassword.style.display = "block";
+            invalidFieldsEmail.style.display = "block";
+            document.getElementById("span-alert").innerHTML="Invalid password"
             span.onclick = function() {
-                invalidFieldsPassword.style.display = "none";
+                invalidFieldsEmail.style.display = "none";
         }
         window.onclick = function(event) {
-            if (event.target == invalidFieldsPassword) {
-            invalidFieldsPassword.style.display = "none";
+            if (event.target == invalidFieldsEmail) {
+            invalidFieldsEmail.style.display = "none";
             }
         }
-        }else{
-            if(inputEmail.value =="" || inputPassword.value ==""){
+        }else if(inputEmail.value =="" || inputPassword.value ==""){
                 inputEmail.classList.add("border-red");
-                inputPassword.classList.add("border-red");
-                invalidFieldsEmpty.style.display = "block";
-                span.onclick = function() {
-                    invalidFieldsEmpty.style.display = "none";
+                invalidFieldsEmail.style.display = "block";
+            document.getElementById("span-alert").innerHTML="Empty fields"
+            span.onclick = function() {
+                invalidFieldsEmail.style.display = "none";
             }
             window.onclick = function(event) {
-                if (event.target == invalidFieldsEmpty) {
-                invalidFieldsEmpty.style.display = "none";
-                }
+            if (event.target == invalidFieldsEmail) {
+            invalidFieldsEmail.style.display = "none";
             }
-            }else{
-                var url = "https://basp-m2022-api-rest-server.herokuapp.com/login?email=" + inputEmail.value + 
-                "&password=" + inputPassword.value;
-                var promise = fetch(url);
-                promise.then(function(response){
-                    console.log(response);
+            }
+        }else{
+                var url = "https://basp-m2022-api-rest-server.herokuapp.com/login?email=" 
+                + inputEmail.value + "&password=" + inputPassword.value;
+                fetch(url)
+                .then(function(response){
                 if(response.status >= 400){
                     throw new Error(response);
                     }else{
@@ -133,15 +115,20 @@ window.onload = function ()  {
                     }
                 })
                 .then(function(data){
-                    console.log(data.msg);
-                    alert (data.msg + "Success "+ "\n" + trueEmail + "\n" + truePassword)
-                    trueEmail= "Email: "
-                    truePassword="Password: "
+                    invalidFieldsEmail.style.display = "block";
+                    document.getElementById("span-alert").innerHTML=(data.msg)
+                    span.onclick = function() {
+                        invalidFieldsEmail.style.display = "none";
+                    }
+                    window.onclick = function(event) {
+                        if (event.target == invalidFieldsEmail) {
+                            invalidFieldsEmail.style.display = "none";
+                        }
+                    }
                 })
                 .catch(function(error){
-                    alert(error + "\n" + "Check Email or Password")
+                    alert(error + "\n" + "Check Email or Password");
                 })
             };
         }
-    }
 }
--- Semana-07/javascript/sign-up.js
+++ Semana-07/javascript/sign-up.js
@@ -13,20 +13,22 @@ window.onload = function ()  {
     var inputSecondPassword = document.getElementById("form-input-rpassword");
     var inputButton = document.getElementById("form-button");
     var textError = document.createElement("p");
-    function test(){
-        var prueba = localStorage.getItem("Birth");
-        var year = prueba.substring(0, prueba.indexOf("/"));
-        var month = prueba.substring(prueba.indexOf("/")+1, prueba.indexOf("/")+3);
-        var day = prueba.substring(prueba.indexOf("/")+4, prueba.indexOf("/") + prueba.length);
-        var dateArray = [day, year, month];
-        var dateStringTest = dateArray.join("-");
-        return dateStringTest;
-    }
-    localStorage.setItem("Birth", test());
+    var invalidFieldsEmail = document.getElementById("invalid-fields-email");
+    var span = document.getElementsByClassName("close");
+     function test(){
+         var prueba = localStorage.getItem("Birth");
+         var year = prueba.substring(0, prueba.indexOf("/"));
+         var month = prueba.substring(prueba.indexOf("/")+1, prueba.indexOf("/")+3);
+         var day = prueba.substring(prueba.indexOf("/")+4, prueba.indexOf("/") + prueba.length);
+         var dateArray = [day, year, month];
+         var dateStringTest = dateArray.join("-");
+         return dateStringTest;
+     }
+    
     inputName.value = localStorage.getItem("Name");
     inputLastName.value = localStorage.getItem("Last name");
     inputDNI.value = localStorage.getItem("ID");
-    inputBirth.value = localStorage.getItem("Birth");
+    inputBirth.value = test();
     inputPhone.value = localStorage.getItem("Phone");
     inputAddress.value = localStorage.getItem("Addres");
     inputCountry.value = localStorage.getItem("Country/city");
@@ -75,19 +77,16 @@ window.onload = function ()  {
             }
         }
     }
-
-
     inputName.onblur = function(){
         if(!onlyLetters(inputName)){
             inputName.classList.add("border-red");
-                textError.classList.add("parragraph-Email")
                 textError.innerHTML = "Verify name";
-                inputName.parentNode.insertBefore(textError, inputName.nextSibling)
+                inputName.parentNode.insertBefore(textError, inputName.nextSibling);
         }else if(inputName.value.length < 3){
             inputName.classList.add("border-red");
-            textError.classList.add("parragraph-Email")
+            textError.classList.add("parragraph-Email");
             textError.innerHTML = "Not enough characters";
-            inputName.parentNode.insertBefore(textError, inputName.nextSibling)
+            inputName.parentNode.insertBefore(textError, inputName.nextSibling);
         }else{
             inputName.classList.add("border-green");
         }
@@ -95,14 +94,11 @@ window.onload = function ()  {
     inputName.onfocus = function(){
             inputName.value = "";
             inputName.classList.remove("border-red");
-            textError.remove()
-        }
-
-
+            textError.remove();
+    }
     inputLastName.onblur = function(){
         if(!onlyLetters(inputLastName)){
             inputLastName.classList.add("border-red");
-                textError.classList.add("parragraph-Email")
                 textError.innerHTML = "Verify last name";
                 inputLastName.parentNode.insertBefore(textError, inputLastName.nextSibling)
         }else if(inputLastName.value.length < 3){
@@ -118,23 +114,19 @@ window.onload = function ()  {
             inputLastName.value = "";
             inputLastName.classList.remove("border-red");
             textError.remove()
-        }
+    }
     
-
     inputDNI.onblur = function(){
         if(!onlyNumbers(inputDNI)){
             inputDNI.classList.add("border-red");
-                textError.classList.add("parragraph-Email")
                 textError.innerHTML = "Verify ID";
                 inputDNI.parentNode.insertBefore(textError, inputDNI.nextSibling)
         }else if(inputDNI.value.length < 7){
             inputDNI.classList.add("border-red");
-            textError.classList.add("parragraph-Email")
             textError.innerHTML = "Not enough characters";
             inputDNI.parentNode.insertBefore(textError, inputDNI.nextSibling)
         }else if(inputDNI.value.length > 8){
             inputDNI.classList.add("border-red");
-            textError.classList.add("parragraph-Email")
             textError.innerHTML = "Check ID number length";
             inputDNI.parentNode.insertBefore(textError, inputDNI.nextSibling)
         }else{
@@ -145,31 +137,24 @@ window.onload = function ()  {
             inputDNI.value = "";
             inputDNI.classList.remove("border-red");
             textError.remove()
-        }
-    
-
+    }
     inputBirth.onblur = function(){
         if(inputBirth.value == ""){
             inputBirth.classList.add("border-red");
-            textError.classList.add("parragraph-Email");
             textError.innerHTML = "Verify date";
             inputBirth.parentNode.insertBefore(textError, inputBirth.nextSibling);
         }else{
             inputBirth.classList.add("border-green");
         }
     }
-    
     inputBirth.onfocus = function(){
         inputBirth.value = "";
         inputBirth.classList.remove("border-red");
         textError.remove();
     }
-
-
     inputPhone.onblur = function(){
         if(!onlyNumbers(inputPhone)){
             inputPhone.classList.add("border-red");
-                textError.classList.add("parragraph-Email")
                 textError.innerHTML = "Verify phone number";
                 inputPhone.parentNode.insertBefore(textError, inputPhone.nextSibling)
         }else if(inputPhone.value.length < 10){
@@ -185,18 +170,14 @@ window.onload = function ()  {
             inputPhone.value = "";
             inputPhone.classList.remove("border-red");
             textError.remove()
-        }
-
-
+    }
     inputAddress.onblur = function(){
         if(!onlyNumbersAndLettersAddress(inputAddress)){
             inputAddress.classList.add("border-red");
-                textError.classList.add("parragraph-Email")
                 textError.innerHTML = "Verify Address log";
                 inputAddress.parentNode.insertBefore(textError, inputAddress.nextSibling)
         }else if(inputAddress.value.length < 3){
             inputAddress.classList.add("border-red");
-            textError.classList.add("parragraph-Email")
             textError.innerHTML = "Not enough characters";
             inputAddress.parentNode.insertBefore(textError, inputAddress.nextSibling)
         }else if (!inputAddress.value.includes(" ")){
@@ -209,18 +190,14 @@ window.onload = function ()  {
             inputAddress.value = "";
             inputAddress.classList.remove("border-red");
             textError.remove()
-        }
-
-
+    }
     inputCountry.onblur = function(){
         if(!onlyNumbersAndLetters(inputCountry)){
             inputCountry.classList.add("border-red");
-                textError.classList.add("parragraph-Email")
                 textError.innerHTML = "Verify country log";
                 inputCountry.parentNode.insertBefore(textError, inputCountry.nextSibling)
         }else if(inputCountry.value.length < 3){
             inputCountry.classList.add("border-red");
-            textError.classList.add("parragraph-Email")
             textError.innerHTML = "Not enough characters";
             inputCountry.parentNode.insertBefore(textError, inputCountry.nextSibling)
         }else{
@@ -231,18 +208,14 @@ window.onload = function ()  {
             inputCountry.value = "";
             inputCountry.classList.remove("border-red");
             textError.remove()
-        }
-
-
+    }
     inputPostal.onblur = function(){
         if(!onlyNumbers(inputPostal)){
             inputPostal.classList.add("border-red");
-                textError.classList.add("parragraph-Email")
                 textError.innerHTML = "Verify postal number";
                 inputPostal.parentNode.insertBefore(textError, inputPostal.nextSibling)
         }else if(inputPostal.value.length <= 4 && inputPostal.value.length >= 5){
             inputPostal.classList.add("border-red");
-            textError.classList.add("parragraph-Email")
             textError.innerHTML = "Not enough characters";
             inputPostal.parentNode.insertBefore(textError, inputPostal.nextSibling)
         }else{
@@ -253,19 +226,15 @@ window.onload = function ()  {
             inputPostal.value = "";
             inputPostal.classList.remove("border-red");
             textError.remove()
-        }
-
-
+    }
     var errorTextEmail = document.createElement("p");
     inputEmail.onblur = function(){
         if(!inputEmail.value.match(emailExpression)){
             inputEmail.classList.add("border-red");
-            errorTextEmail.classList.add("parragraph-Email")
             errorTextEmail.innerHTML = "Verify email";
             inputEmail.parentNode.insertBefore(errorTextEmail, inputEmail.nextSibling);
         }else if(inputEmail.value.length < 3){
             inputEmail.classList.add("border-red");
-            errorTextEmail.classList.add("parragraph-Email");
             errorTextEmail.innerHTML = "Not enough characters";
             inputEmail.parentNode.insertBefore(errorTextEmail, inputEmail.nextSibling);
             alert("Not enough characters");
@@ -273,23 +242,18 @@ window.onload = function ()  {
             inputEmail.classList.add("border-green")
         }
     }
-    
     inputEmail.onfocus = function(){
         inputEmail.value = "";
         inputEmail.classList.remove("border-red");
         errorTextEmail.remove();
     }
-
-
     inputPassword.onblur = function(){
         if(!onlyNumbersAndLetters(inputPassword)){
             inputPassword.classList.add("border-red");
-                textError.classList.add("parragraph-Email")
                 textError.innerHTML = "Verify password";
                 inputPassword.parentNode.insertBefore(textError, inputPassword.nextSibling)
         }else if(inputPassword.value.length < 8){
             inputPassword.classList.add("border-red");
-            textError.classList.add("parragraph-Email")
             textError.innerHTML = "Not enough characters";
             inputPassword.parentNode.insertBefore(textError, inputPassword.nextSibling)
         }else{
@@ -300,33 +264,26 @@ window.onload = function ()  {
             inputPassword.value = "";
             inputPassword.classList.remove("border-red");
             textError.remove()
-        }
-
-
+    }
     var errorTextPaswordConfirm = document.createElement("p");
     inputSecondPassword.onblur = function(){
         if(!inputSecondPassword.value === inputPassword){
             inputSecondPassword.classList.add("border-red");
-            errorTextPaswordConfirm.classList.add("parragraph-Email")
             errorTextPaswordConfirm.innerHTML = "Use same password";
             inputSecondPassword.parentNode.insertBefore(errorTextPaswordConfirm, inputSecondPassword.nextSibling);
         }else if(inputSecondPassword.value.length === ""){
             inputSecondPassword.classList.add("border-red");
-            errorTextPaswordConfirm.classList.add("parragraph-Email");
             errorTextPaswordConfirm.innerHTML = "Use same password";
             inputSecondPassword.parentNode.insertBefore(errorTextPaswordConfirm, inputSecondPassword.nextSibling);
         }else{
             inputSecondPassword.classList.add("border-green");
         }
     }
-    
     inputSecondPassword.onfocus = function(){
         inputSecondPassword.value = "";
         inputSecondPassword.classList.remove("border-red");
         errorTextPaswordConfirm.remove();
     }
-
-
     inputButton.onclick = function(e){
         e.preventDefault()
         var year = inputBirth.value.substring(0, inputBirth.value.indexOf("-"));
@@ -334,22 +291,36 @@ window.onload = function ()  {
         var day = inputBirth.value.substring(inputBirth.value.indexOf("-")+4, inputBirth.value.length);
         var dateArray = [month, day, year];
         var dateString = dateArray.join("/");
-        console.log(dateString);
-        console.log(year);
-        console.log(day);
-        console.log(month);
         if(inputName.classList.contains("border-red") ||inputLastName.classList.contains("border-red")
         ||inputDNI.classList.contains("border-red")||inputBirth.classList.contains("border-red")
         ||inputAddress.classList.contains("border-red")||inputCountry.classList.contains("border-red")
         ||inputPhone.classList.contains("border-red")||inputPostal.classList.contains("border-red")
         ||inputPassword.classList.contains("border-red")||inputSecondPassword.classList.contains("border-red")
         ||inputEmail.classList.contains("border-red")){
-            alert("Invalid data")
+            invalidFieldsEmail.style.display = "block";
+            document.getElementById("span-alert").innerHTML=("One or more fields are")
+            span.onclick = function() {
+                invalidFieldsEmail.style.display = "none";
+            }
+            window.onclick = function(event) {
+                if (event.target == invalidFieldsEmail) {
+                    invalidFieldsEmail.style.display = "none";
+                }
+            }
         }else if(inputName.value ==="" || inputLastName.value ==="" || inputDNI.value ==="" 
         || inputBirth.value ==="" || inputAddress.value ==="" || inputCountry.value ==="" 
         || inputPhone.value ==="" || inputPostal.value ==="" || inputPassword.value ==="" 
         || inputEmail.value ==="" || inputSecondPassword.value ===""){
-            alert("Fill requierd fields")
+            invalidFieldsEmail.style.display = "block";
+            document.getElementById("span-alert").innerHTML=("Empty fields")
+            span.onclick = function() {
+                invalidFieldsEmail.style.display = "none";
+            }
+            window.onclick = function(event) {
+                if (event.target == invalidFieldsEmail) {
+                    invalidFieldsEmail.style.display = "none";
+                }
+            }
         }
         else{
             var url = "https://basp-m2022-api-rest-server.herokuapp.com/signup?name=" + inputName.value + 
@@ -361,13 +332,16 @@ window.onload = function ()  {
                 return response.json();
             })
             .then(function(data){
-                alert(data.mgs + "\n" + "Name :" + inputName.value +"\n"
-                + "Last name: " + inputLastName.value+"\n" 
-            + "ID :" + inputDNI.value +"\n" + "Birth date :" + inputBirth.value +"\n" 
-            + "Phone number :" + inputPhone.value +"\n" + "Address :" + inputAddress.value + "\n"
-            + "City/Country :" + inputCountry.value +"\n" + "Postal code" + inputPostal.value + "\n"
-            + "Email :" + inputEmail.value +"\n" + "Password: " + inputPassword.value +"\n"
-            + "Password confirmation: " + inputSecondPassword.value);
+                invalidFieldsEmail.style.display = "block";
+                    document.getElementById("span-alert").innerHTML=(data.msg)
+                    span.onclick = function() {
+                        invalidFieldsEmail.style.display = "none";
+                    }
+                    window.onclick = function(event) {
+                        if (event.target == invalidFieldsEmail) {
+                            invalidFieldsEmail.style.display = "none";
+                        }
+                    }
                 localStorage.setItem("Name", inputName.value);
                 localStorage.setItem("Last name", inputLastName.value);
                 localStorage.setItem("ID", inputDNI.value);
